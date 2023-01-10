05d473434d4d0b7607640a21942cac0813f29948
mi-fasol <minyoong39@naver.com>
2023-01-10 16:08:43 +0900

    feat: 로딩 화면 추가

---
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.instagram
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, HomePage::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}

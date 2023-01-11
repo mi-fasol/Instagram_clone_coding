a15fe0ac7715681860f910425cdbe543946e6577
mi-fasol <minyoong39@naver.com>
2023-01-11 11:25:10 +0900

    feat: 각 페이지 별 프래그먼트 추가

---
+++ app/src/main/java/com/example/instagram/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.instagram
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+

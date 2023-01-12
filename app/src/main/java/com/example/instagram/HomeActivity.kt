b5f304df0c28e85a88dc1b16d5ebe1ec6081f18e
mi-fasol <minyoong39@naver.com>
2023-01-12 11:13:20 +0900

    feat: 로그아웃 기능 추가

--- app/src/main/java/com/example/instagram/HomeActivity.kt
+++ app/src/main/java/com/example/instagram/HomeActivity.kt
@@ -12,11 +12,12 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        var bottom_nav = findViewById(R.id.bnv_main) as BottomNavigationView
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bnv_main)
 
-        var postBtn : Button = findViewById(R.id.addPost)
-        var heartBtn : Button = findViewById(R.id.heart)
-        var chatBtn : Button = findViewById(R.id.dm)
+        var postBtn: Button = findViewById(R.id.addPost)
+        var heartBtn: Button = findViewById(R.id.heart)
+        var chatBtn: Button = findViewById(R.id.dm)
+
 
         postBtn.setOnClickListener {
             val intent = Intent(this, PostActivity::class.java)
@@ -36,24 +37,28 @@ class HomeActivity : AppCompatActivity() {
             finish()
         }
 
-        bottom_nav.run{ setOnNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.nav_home ->{
-                    val homeFragment = HomeFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, homeFragment).commit()
+        bottomNav.run {
+            setOnNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.nav_home -> {
+                        val homeFragment = HomeFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fl_container, homeFragment).commit()
+                    }
+                    R.id.nav_search -> {
+                        val searchFragment = SearchFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fl_container, searchFragment).commit()
+                    }
+                    else -> {
+                        val myPageFragment = MyPageFragment()
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fl_container, myPageFragment).commit()
+                    }
                 }
-                R.id.nav_search ->{
-                    val searchFragment = SearchFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, searchFragment).commit()
-                }
-                else -> {
-                    val myPageFragment = MyPageFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, myPageFragment).commit()
-                }
+                true
             }
-            true
-        }
-        selectedItemId = R.id.nav_home
+            selectedItemId = R.id.nav_home
         }
     }
 }

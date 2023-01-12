ad91769476688d51fb53b86760bc144f970d2331
mi-fasol <minyoong39@naver.com>
2023-01-12 14:18:45 +0900

    feat: 마이 페이지 바텀 시트 추가

--- app/src/main/java/com/example/instagram/BottomSheet.kt
+++ app/src/main/java/com/example/instagram/BottomSheet.kt
@@ -2,8 +2,6 @@ package com.example.instagram
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,20 +9,25 @@ import android.widget.Button
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
 
-class MyPageFragment : Fragment() {
+class BottomSheet() : BottomSheetDialogFragment() {
     private var googleSignInClient: GoogleSignInClient? = null
     private var auth: FirebaseAuth? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return inflater.inflate(R.layout.bottom_sheet_dialog, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
@@ -33,21 +36,11 @@ class MyPageFragment : Fragment() {
 
         googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
 
-        val view: View = inflater!!.inflate(R.layout.fragment_my_page, container, false)
-
-        view.findViewById<Button>(R.id.signOutBtn)?.setOnClickListener{
+        view?.findViewById<Button>(R.id.signOutBtn)?.setOnClickListener{
+            dialog?.dismiss()
             signOut()
         }
 
-        view.findViewById<Button>(R.id.menu).setOnClickListener {
-            val bottomSheet = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
-            val bottomSheetView = BottomSheetDialog(requireContext())
-            bottomSheetView.setContentView(bottomSheet)
-            bottomSheetView.show()
-        }
-
-        // Return the fragment view/layout
-        return view
     }
 
     private fun signOut() {

29857029efc79cbea83c08fb4520509517c3551c
mi-fasol <minyoong39@naver.com>
2023-01-14 21:47:19 +0900

    feat: 댓글 페이지 네비게이션 추가

--- app/src/main/java/com/example/instagram/Scenarios/main/post/CommentActivity.kt
+++ app/src/main/java/com/example/instagram/Scenarios/main/post/CommentActivity.kt
@@ -1,11 +1,14 @@
 package com.example.instagram.Scenarios.main.post
 
+import android.content.Intent
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram.Adapter.CommentAdapter
 import com.example.instagram.Data.ImgData
 import com.example.instagram.R
+import com.example.instagram.Scenarios.main.HomeActivity
 
 class CommentActivity : AppCompatActivity() {
     lateinit var commentAdapter: CommentAdapter
@@ -14,6 +17,14 @@ class CommentActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         setContentView(R.layout.activity_comment)
+
+        var back : Button = findViewById(R.id.backNav)
+
+        back.setOnClickListener {
+            var intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         cImg.add(ImgData(getDrawable(R.drawable.img)!!))
         cImg.add(ImgData(getDrawable(R.drawable.dog)!!))

bb754052b485c67af7c9f81b5a09ec51415f479a
mi-fasol <minyoong39@naver.com>
2023-01-17 14:28:38 +0900

    feat: Id와 Nickname 입력 시 필터 제한 추가

--- app/src/main/java/com/example/instagram/Scenarios/intro/RegisterActivity.kt
+++ app/src/main/java/com/example/instagram/Scenarios/intro/RegisterActivity.kt
@@ -4,13 +4,16 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.instagram.Data.UserSharedPreferences
 import com.example.instagram.R
 import com.example.instagram.Scenarios.main.HomeActivity
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -27,7 +30,30 @@ class RegisterActivity : AppCompatActivity() {
         var loginButton: Button = findViewById(R.id.registerComplete)
         var userPref = UserSharedPreferences
 
+        var filterId = InputFilter { source, start, end, dest, dstart, dend ->
+            val ps = Pattern.compile("^[_.a-z0-9]+$")
+            if (!ps.matcher(source).matches()) {
+                return@InputFilter ""
+            } else{
+                Toast.makeText(this, "영어 소문자 및 숫자, 특수기호 _ .만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
+                return@InputFilter null
+            }
+        }
+
+        var filterNick = InputFilter { source, start, end, dest, dstart, dend ->
+            val ps = Pattern.compile("^[a-zA-Z0-9ㄱ-ㅎ가-흐ㄱ-ㅣ가-힣]+$")
+            if (!ps.matcher(source).matches()) {
+                return@InputFilter ""
+            } else{
+                Toast.makeText(this, "한글 및 영어만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
+                return@InputFilter null
+            }
+        }
+
         loginButton.isEnabled = false
+
+        editId.filters = arrayOf(filterId)
+        editNick.filters = arrayOf(filterNick)
 
         editId.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -54,20 +80,18 @@ class RegisterActivity : AppCompatActivity() {
             override fun afterTextChanged(p0: Editable?) {}
         })
 
-        if (loginButton.isEnabled)
-            loginButton.setOnClickListener {
-                userPref.setUserNick(this, userNick)
-                userPref.setUserId(this, userId)
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-                finish()
-
-            }
-
-        backTo.setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
+        loginButton.setOnClickListener {
+            userPref.setUserNick(this, userNick)
+            userPref.setUserId(this, userId)
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
+
+            backTo.setOnClickListener {
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
